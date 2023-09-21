15ad5371567d2eab93fdb305dd1b5da2ca2a1d08
omarbessi04 <omarbessi04@gmail.com>
2023-09-21 15:25:58 +0000

    Check for player input and move player

--- TileTraveller.py
+++ TileTraveller.py
@@ -10,6 +10,7 @@ until you start to see a way to convert this description into code.
 x = 1
 y = 1
 coordinates_of_player = [x,y]
+Invalid_direction = True
 
 #functions that move player
 def goNorth():
@@ -27,23 +28,64 @@ def goWest():
 #list of all the tiles that are available, N, S, E, W
 directions = [[[1,1][1,2][2,1][3,1][3,2]], [[1,2][1,3][2,2][3,3][3,2]], [[1,3][1,2][2,3]], [[3,3][2,3][2,2]]]
 
-available_direction = []
 while coordinates_of_player != [3, 1]:
-    available_direction = []
-#Checks_available_directions
+    N, S, E, W = False
+    Valid_directions = []
+    
+    #Checks_Valid_directions
     for i in range(len(directions)):
         if coordinates_of_player in (directions)[i]:
+
             if (directions)[i] == directions[0]:
-                available_direction.append("(N)orth")
+                N = True
+                Valid_directions.append("(N)orth")
+
             if (directions)[i] == directions[1]:
-                available_direction.append("(S)outh")
+                S = True
+                Valid_directions.append("(S)outh")
+
             if (directions)[i] == directions[2]:
-                available_direction.append("(E)ast")
+                E = True
+                Valid_directions.append("(E)ast")
+
             if (directions)[i] == directions[3]:
-                available_direction.append("(W)est")
+                W = True
+                Valid_directions.append("(W)est")
 
+    print("You can travel: ", " or ".join(Valid_directions))
+    player_move = input("Direction: ")
 
-                
+    if player_move.lower() == "n":
+        if N:
+            goNorth()
+            Invalid_direction = False
+        else:
+            Invalid_direction = True
 
-        
+    elif player_move.lower() == "s":
+        if S:
+            goSouth()
+            Invalid_direction = False
+        else:
+            Invalid_direction = True
 
+    elif player_move.lower() == "e":
+        if E:
+            goEast()
+            Invalid_direction = False
+        else:
+            Invalid_direction = True
+
+    elif player_move.lower() == "w":
+        if W:
+            goWest()
+            Invalid_direction = False
+        else:
+            Invalid_direction = True
+
+    if Invalid_direction:
+        print("Not a valid direction!")
+    else:
+        Valid_directions = []
+
+print("Victory!")
