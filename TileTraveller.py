211f1bf5b805f39dd3afc7f89b16c34609127c2f
CoolSmurf27 <gardarsolvi@gmail.com>
2023-09-21 13:13:01 +0000

    verkefna_description

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,1 +1,8 @@
-print("hello world")
+"""
+1. Describe the general outline of how you're going to develop this program, and
+commit it.
+2. Then gradually break it down into more detailed steps (committing after each
+comprehensive step),
+until you start to see a way to convert this description into code.
+"""
+# 3. Then start writing the code
