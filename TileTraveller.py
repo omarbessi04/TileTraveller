0a52c13501289d023129b40f6a4394623f0b7119
CoolSmurf27 <gardarsolvi@gmail.com>
2023-09-21 14:52:16 +0000

    Checks_available_directions

--- TileTraveller.py
+++ TileTraveller.py
@@ -27,5 +27,23 @@ def goWest():
 #list of all the tiles that are available, N, S, E, W
 directions = [[[1,1][1,2][2,1][3,1][3,2]], [[1,2][1,3][2,2][3,3][3,2]], [[1,3][1,2][2,3]], [[3,3][2,3][2,2]]]
 
+available_direction = []
 while coordinates_of_player != [3, 1]:
-    
+    available_direction = []
+#Checks_available_directions
+    for i in range(len(directions)):
+        if coordinates_of_player in (directions)[i]:
+            if (directions)[i] == directions[0]:
+                available_direction.append("(N)orth")
+            if (directions)[i] == directions[1]:
+                available_direction.append("(S)outh")
+            if (directions)[i] == directions[2]:
+                available_direction.append("(E)ast")
+            if (directions)[i] == directions[3]:
+                available_direction.append("(W)est")
+
+
+                
+
+        
+
